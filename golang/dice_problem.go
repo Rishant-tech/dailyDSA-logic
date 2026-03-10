9eed4b2429170ca24e3dc9f30819b10d9c956a55
Rishant Rajpoot <raj.rishi4352@gmail.com>
2026-03-10 21:35:19 +0530

    Add functions to all problem files and call them in main.go

--- golang/dice_problem.go
+++ golang/dice_problem.go
@@ -1,1 +1,8 @@
 package main
+
+func DiceProbability(sides int) float64 {
+	if sides <= 0 {
+		return 0
+	}
+	return 1.0 / float64(sides)
+}
--- golang/main.go
+++ golang/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Check Even or Odd (5):", CheckEvenOrOdd(5))
+	fmt.Println("Closest Number ([1, 3, 7, 8], 5):", ClosestNumber([]int{1, 3, 7, 8}, 5))
+	fmt.Println("Dice Probability (6 sides):", DiceProbability(6))
+	fmt.Println("Multiplication Table (3):", MultiplicationTable(3))
+	fmt.Println("Sum of Naturals (10):", SumOfNaturals(10))
+	fmt.Println("Sum of Squares (5):", SumOfSquares(5))
+	a, b := SwapTwoNumbers(10, 20)
+	fmt.Println("Swap Two Numbers (10, 20):", a, b)
+}
