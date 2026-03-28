9847e346fd25b9c46320bb997df12b3dee61cc32
Rishant Rajpoot <78777458+Rishant-tech@users.noreply.github.com>
2026-03-28 18:09:47 +0530

    Update leetcode-problems.go

--- golang/leetcode-problems.go
+++ golang/leetcode-problems.go
@@ -67,3 +67,21 @@ func majorityElement(nums []int) int {
     }
     return mj
 }
+
+
+/*
+Two Sum problem
+*/
+
+//brute force
+
+func twoSum(nums []int, target int) []int {
+    for i:=0 ; i<len(nums);i++{
+        for j:=i+1; j<len(nums);j++{
+            if nums[i]+nums[j] == target{
+                return []int{i,j}
+            }
+        }
+    }
+    return nil
+}
