b283cd40bc27976955ecd3a079220e6f3afd7228
Rishant Rajpoot <rishant.rajpoot21@gmail.com>
2026-06-01 08:01:16 +0530

    main

--- practise/main.go
+++ practise/main.go
@@ -1,80 +1,65 @@
 package main
 
+/*
+Problem: Longest Subarray with Sum K
+
+Description:
+Given an array of integers arr and an integer k, write a function in Go to find the length of the longest contiguous subarray whose sum equals k.
+
+Example:
+arr := []int{1, -1, 5, -2, 3}
+k := 3
+Output: 4  // The subarray [1, -1, 5, -2] sums to 3
+
+arr := []int{-2, -1, 2, 1}
+k := 1
+Output: 2  // The subarray [-1, 2] sums to 1
+
+*/
+
 import (
 	"fmt"
 )
 
 func main() {
-	num := 0
-	fmt.Println("Enter the number to find frequency in an array : ")
-	// fmt.Scan(&num)
-	arr := []int{0,1,1,1,5,5,2,2,4,4}
-	freq := findFrequencyB(arr, num)
-	fmt.Printf("Brute Frequency of a given number %d, exists %d times\n", num, freq)
-	num = 6
-	arr = []int{1,2,3,4,5,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,7,7,7,8,8,8,9}
-	freqO := findFrequencyO(arr, num)
-	fmt.Printf("Optimized Frequency of a given number %d, exists %d times\n", num, freqO)
+	arr := []int{1, -1, 5, -2, 3}
+	var num int
+	fmt.Println("Enter the num : ")
+	fmt.Scan(&num)
+
+	subArrSize, subArr := findSubArr(arr, num)
+
+	fmt.Printf("The size of sub array for sum %d, is %d \n", num, subArrSize)
+	fmt.Println("The sub Arr : ", subArr)
 }
 
-//can be applied in any of the array
-func findFrequencyB(arr []int, num int) int {
-	f := 0
-	for i := 0 ; i<len(arr); i++ {
-		if arr[i] == num {
-			f +=1
+func findSubArr(arr []int, num int) (int, []int){
+	preFix := make(map[int]int)
+	sum := 0
+	maxLen := 0 
+	start := 0
+
+	//{1, -1, 5, -2, 3} 0,1,2,3,4 //3
+
+	for i := 0; i < len(arr); i++ {
+		sum += arr[i]
+
+		if sum == num { //num 3 true for 3
+			maxLen = i+1
 		}
-	}
-	return f
-}
 
-//due to linear scan, only O(n) time complexity and O(1) space complexity
-
-//applied in only sorted array
-func findFrequencyO(arr []int, num int) int {
-	
-	firstIndex := findFirst(arr, num)
-	if firstIndex == -1 {
-		return 0
-	}
-	lastIndex := findLast(arr, num)
-
-	return lastIndex-firstIndex+1
-}
-
-func findFirst(arr []int, num int) int {
-	res := -1
-	l, r := 0, len(arr)-1
-
-	for l <= r {
-		mid := l+(r-l)/2
-		if arr[mid] == num {
-			res = mid
-			r = mid-1
-		}else if arr[mid] < num {
-			l = mid +1
-		}else {
-			r = mid -1
+		if idx, found := preFix[sum - num]; found {
+			lenn := i - idx
+			if lenn > maxLen {
+				maxLen = lenn
+				start += 1
+			}
 		}
-	}
-
-	return res
-}
-
-func findLast(arr []int, num int) int {
-	res := 0
-	l, r := 0, len(arr)-1
-
-	for l <= r {
-		mid := l+(r-l)/2
-		if arr[mid] == num {
-			res = mid
-			l = mid + 1
-		}else if arr[mid] < num {
-			l = mid +1
-		}else {
-			r = mid -1
+		if _, exists := preFix[sum]; !exists {
+			preFix[sum] = i 
 		}
+
 	}
-	return res
+	return maxLen, arr[start:maxLen]
+	//map - [-2,0], [-1,1], [5,2], 
 }
