040574546a03ddb9403f92d618f96f53733a4340
Rishant Rajpoot <rishant.rajpoot21@gmail.com>
2026-06-02 12:34:09 +0530

    order and payment

--- practise/main.go
+++ practise/main.go
@@ -1,65 +1,88 @@
-package main
+package main 
 
 /*
-Problem: Longest Subarray with Sum K
-
-Description:
-Given an array of integers arr and an integer k, write a function in Go to find the length of the longest contiguous subarray whose sum equals k.
-
-Example:
-arr := []int{1, -1, 5, -2, 3}
-k := 3
-Output: 4  // The subarray [1, -1, 5, -2] sums to 3
-
-arr := []int{-2, -1, 2, 1}
-k := 1
-Output: 2  // The subarray [-1, 2] sums to 1
-
+Order
+1 100
+2 200
+3 150
+ 
+ 
+Payments
+1 100
+2 50
+2 150
+4 80
+ 
+Expected Output
+ 
+1 MATCHED
+2 MATCHED
+3 MISSING_PAYMENT
+4 MISSING_ORDER9
 */
 
 import (
 	"fmt"
 )
 
-func main() {
-	arr := []int{1, -1, 5, -2, 3}
-	var num int
-	fmt.Println("Enter the num : ")
-	fmt.Scan(&num)
-
-	subArrSize, subArr := findSubArr(arr, num)
-
-	fmt.Printf("The size of sub array for sum %d, is %d \n", num, subArrSize)
-	fmt.Println("The sub Arr : ", subArr)
+type Orders struct{
+	id int
+	amount int
 }
 
-func findSubArr(arr []int, num int) (int, []int){
-	preFix := make(map[int]int)
-	sum := 0
-	maxLen := 0 
-	start := 0
+type Payments struct {
+	id int
+	amount int
+}
 
-	//{1, -1, 5, -2, 3} 0,1,2,3,4 //3
+func main() {
 
-	for i := 0; i < len(arr); i++ {
-		sum += arr[i]
+	orders := []Orders{
+		{1,50}, //t - 1
+		{2,200},
+		{3,150},
+	}
 
-		if sum == num { //num 3 true for 3
-			maxLen = i+1
-		}
+	payments := []Payments{
+		{1,100},
+		{2,50},
+		{2,150},
+		{4,80},
+	}
 
-		if idx, found := preFix[sum - num]; found {
-			lenn := i - idx
-			if lenn > maxLen {
-				maxLen = lenn
-				start += 1
+
+	//handle missing payment and matched payment vlaues for order id
+	for _, order := range orders {
+		totalAmount := 0
+
+		for _, payment := range payments{
+			if payment.id == order.id{
+				totalAmount += payment.amount
 			}
 		}
-		if _, exists := preFix[sum]; !exists {
-			preFix[sum] = i 
-		}
 
+		if totalAmount == order.amount {
+			fmt.Println(order.id,"MATCHED")
+		} else {
+			fmt.Println(order.id,"MISSING_PAYMENT")
+		}
 	}
-	return maxLen, arr[start:maxLen]
-	//map - [-2,0], [-1,1], [5,2], 
+
+	//handle missing order id, id payment exists for that
+	for  _, payment := range payments {
+
+		found := false //flag to keep track of payment correspoinding to order id
+
+		for  _, order := range orders {
+			if order.id == payment.id {
+				found = true
+				break
+			} 
+		}
+		
+		if !found {
+			fmt.Println(payment.id, "MISSING_ORDER")
+		}
+	}
+
 }
